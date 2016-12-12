e74a23f6319bca6032964f75329ecae462a37a05
daria.minina@cactussoft.biz <daria.minina@cactussoft.biz>
2016-12-12 18:09:37 +0300

    rewrite login tests

--- WAG_tests/WAG_tests/TestBase.cs
+++ WAG_tests/WAG_tests/TestBase.cs
@@ -13,18 +13,67 @@ namespace WAG_tests
     [TestFixture()]
     public class TestBase
     {
+      protected  FirefoxDriver firefox;
+
         [SetUp]
-        public void StartApplication()
+        public void StartBrowser()
         {
-            string browserType = System.Environment.GetEnvironmentVariable("BROWSER");
-            string baseUrl = System.Environment.GetEnvironmentVariable("BASE_URL");
-            string hubUrl = System.Environment.GetEnvironmentVariable("HUB_URL");
-
-            if (baseUrl == null)
-            {
-                baseUrl = "http://whiteaway.com";
-            }
-
+            firefox = new FirefoxDriver();
+            firefox.Manage().Window.Maximize();
+            firefox.Navigate().GoToUrl("http://whiteaway.com/");
         }
+
+
+        [TearDown]
+        public void StopBrowser()
+        {
+            if (firefox != null)
+            {
+                firefox.Quit();
+                firefox = null;
+            }
+        }
+
+
+
+        protected void Logout()
+        {
+            firefox.FindElement(By.LinkText("Log af")).Click();
+            firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+        }
+
+
+        protected void Login(string login, string pass)
+        {
+            firefox.FindElement(By.LinkText("Log ind")).Click();
+            firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            firefox.FindElement(By.Id("email_address")).SendKeys(login);
+            firefox.FindElement(By.Id("password")).SendKeys(pass);
+            firefox.FindElement(By.XPath("//button[@type='submit']")).Click();
+            firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+
+
+
+        protected bool IsElementPresent(By by)
+        {
+            return firefox.FindElements(by).Count > 0;
+        }
+
+
+        protected bool LoggedInErrorDisplayed()
+        {
+            return IsElementPresent(By.LinkText("Fejl: E-mail -adressen eller kodeordet blev ikke genkendt."));
+        }
+
+
+        protected bool IsLoggedIn()
+        {
+            return IsElementPresent(By.LinkText("Log af"));
+        }
+
+
+
     }
 }
--- WAG_tests/WAG_tests/UITests/UserLogin.cs
+++ WAG_tests/WAG_tests/UITests/UserLogin.cs
@@ -10,71 +10,52 @@ namespace WAG_tests
     [TestFixture()]
     public class UserLogin : TestBase
     {
-        FirefoxDriver firefox;
+       // FirefoxDriver firefox;
 
-    [SetUp]
+   /* [SetUp]
         public void StartBrowser()
         {
             firefox = new FirefoxDriver();
             firefox.Manage().Window.Maximize();
+            firefox.Navigate().GoToUrl("http://whiteaway.com/");
         }
+
+
+    [TearDown]
+    public void StopBrowser()
+    {
+        if (firefox != null)
+        {
+            firefox.Quit();
+            firefox = null;
+        }
+    } */
+
 
         [Test()]
         public void WrongDataLogin()
         {
-             //firefox = new FirefoxDriver();
-           // firefox.Manage().Window.Maximize();
-          // try
-          // {
-               //chrome = new ChromeDriver(@"D:\WAG_tests");
-               firefox.Navigate().GoToUrl("http://whiteaway.com/");
-               firefox.FindElement(By.LinkText("Log ind")).Click();
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
-               firefox.FindElement(By.Id("email_address")).SendKeys("test.yhy@yopmail.com");
-               firefox.FindElement(By.Id("password")).SendKeys("qqq");
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
-               firefox.FindElement(By.XPath("//button[@type='submit']")).Click();
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
-
-               firefox.FindElement(By.CssSelector("div.alert.alert-danger"))
-                   .Text.Equals("Fejl: E-mail -adressen eller kodeordet blev ikke genkendt. ");
-          // }
-          // finally { firefox.Quit(); }
-        }
-
-        [TearDown]
-        public void StopBrowser()
-        {
-            if (firefox != null)
-            {
-                firefox.Quit();
-                firefox = null;
-            }
+               Login("test.yhy@yopmail.com", "qwe");
+            LoggedInErrorDisplayed();
         }
 
 
-
+       
 
        [Test()]
         public void CorrectDataLogin()
         {
-           // firefox = new FirefoxDriver();
-           // firefox.Manage().Window.Maximize();
-
-          // try
-          // {
-               // chrome = new ChromeDriver(@"D:\WAG_tests");
-               firefox.Navigate().GoToUrl("http://prelive.whiteaway.com/");
-               firefox.FindElement(By.LinkText("Log ind")).Click();
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
-               firefox.FindElement(By.Id("email_address")).SendKeys("test.yhy@yopmail.com");
-               firefox.FindElement(By.Id("password")).SendKeys("l5qjW");
-               firefox.FindElement(By.XPath("//button[@type='submit']")).Click();
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
-               firefox.FindElement(By.LinkText("Log af")).Click();
-               firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
-           //} finally { firefox.Quit(); }
+               Login("test.yhy@yopmail.com", "l5qjW");
+               IsLoggedIn();
+               Logout();
         }
+
+
+
+
+
+     
+
 
     }
 }
